136be17be3cf14f6f0b9470d5e7b7e2339be8988
MaximGolub <maks.golub2@gmail.com>
2022-03-08 19:14:15 +0200

    Update script.js

--- hm9/script.js
+++ hm9/script.js
@@ -6,25 +6,24 @@ const BORDER_WIDTH = 50;
 
 let ballActive = false;
 
-function onMove(e){
+function onMove(e) {
     if (!ballActive) return;
 
-	const { clientX, clientY } = e;
-	const left = clientX - sceneRect.left - BORDER_WIDTH - ballRect.width / 2;
-	const top = clientY - sceneRect.top - BORDER_WIDTH - ballRect.height / 2;
+    const { clientX, clientY } = e;
+    const left = clientX - sceneRect.left - BORDER_WIDTH - ballRect.width / 2;
+    const top = clientY - sceneRect.top - BORDER_WIDTH - ballRect.height / 2;
     const right = scene.offsetWidth - scene.offsetTop - ballRect.width;
     const bottom = scene.offsetHeight - scene.offsetTop - ballRect.width;
 
-    if(left < 0 || left > right || top < 0 || top > bottom){
-        document.removeEventListener('mousemove', onMove);
+    if (left < 0 || left > right || top < 0 || top > bottom) {
         return;
     }
 
-	ball.style.transform = `translate(${left}px, ${top}px)`;
+    ball.style.transform = `translate(${left}px, ${top}px)`;
 }
 
 scene.addEventListener("mousemove", onMove);
 
 ball.addEventListener("click", () => {
-	ballActive = !ballActive;
+    ballActive = !ballActive;
 });
